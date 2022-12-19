787a83fb20387095cc567c017e837f6f471e659f
emohex <olumidemm@gmail.com>
2022-12-19 21:04:16 +0100

    get end point

---
+++ dotnet-tut/Controllers/villaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using dotnet_tut.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_tut.Controllers
+{
+    [Route("api/v1/villapi")]
+    [ApiController]
+    public class villaAPIController: ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa{Id=1, Name="olumide villa one"},
+                new Villa{Id=2, Name="new villa two"}
+            };
+        }
+    }
+}
---
+++ dotnet-tut/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace dotnet_tut.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
